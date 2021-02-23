48a4b3bbe2cb5e354d7aecf11d008d9aa81286dd
= <strandberg.benjamin@gmail.com>
2021-02-23 18:43:37 +0100

    test

--- Main.hs
+++ Main.hs
@@ -16,7 +16,7 @@ main = do
     Gtk.init Nothing 
 
     win <- new Gtk.Window[#title := "Journal System"]
-    on win #destroy Gtk.mainQuit 
+    
     #resize win 640 480
     #setPosition win Gtk.WindowPositionCenter 
 
@@ -49,6 +49,13 @@ main = do
     loginBtn <- new Gtk.Button [#label := "Login"]
     #add vertBox loginBtn
 
+    exitBtn <- new Gtk.Button [#label := "Exit"]
+    #add vertBox exitBtn
+
+    on exitBtn #clicked Gtk.mainQuit 
+
+    
+
     on loginBtn #clicked $ do 
         userIn <- Gtk.entryGetText usernameEntry
         passIn <- Gtk.entryGetText passEntry
@@ -65,8 +72,37 @@ main = do
 
 invokeMenuScreen :: Gtk.Window  -> IO()
 invokeMenuScreen win = do 
-    menuState <- new Gtk.ApplicationWindow [#title := "Journal System"]
-    on menuState #destroy Gtk.mainQuit 
+    Gtk.widgetDestroy win
+
+    
+    menuState <- new Gtk.Window [#title := "Journal System"]
+
     #resize menuState 640 480
+    #setPosition menuState Gtk.WindowPositionCenter 
+
+    vertBox <- new Gtk.Box [#orientation := Gtk.OrientationVertical]
+    #add menuState vertBox
+
+    addPatientbtn <- new Gtk.Button [#label := "Add patient"]
+    #add vertBox addPatientbtn
+
+    horBox1 <- new Gtk.Box [#orientation := Gtk.OrientationHorizontal]
+    #add vertBox horBox1
+
+    patientEntry <- Gtk.entryNew
+    Gtk.containerAdd horBox1 patientEntry
+
+
+    findRecordBtn <- new Gtk.Button [#label := "Find patient"]
+    #add horBox1 findRecordBtn
+
+    logOutBtn <- new Gtk.Button [#label := "Log out"]
+    #add vertBox logOutBtn
+
+    on logOutBtn #clicked $ do
+        #destroy menuState
+        main
+
+    
     #showAll menuState
 
