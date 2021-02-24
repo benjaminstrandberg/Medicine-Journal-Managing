0f3b5b617dd3d2c711bc99d323dd1841c46de381
= <strandberg.benjamin@gmail.com>
2021-02-24 18:37:46 +0100

    Now possible to add a patient and store its data in a text file

--- Main.hs
+++ Main.hs
@@ -18,6 +18,15 @@ instance Show RhFactor where
     show Pos = "+"
     show Neg = "-"
 
+--                     FName  LName Gender Age Height Weight BT  
+data Patient = Patient {firstName :: String,
+                        lastName :: String,
+                        age :: Int, 
+                        gender :: String,
+                        height :: Double,
+                        weight :: Double,
+                        bloodType :: BloodType} deriving (Show)
+
 
 donateTo :: BloodType -> BloodType -> Bool 
 donateTo _ (BloodType AB _) = True -- Kan ta emot från alla
@@ -26,6 +35,15 @@ donateTo (BloodType A _) (BloodType A _) = True
 donateTo (BloodType B _) (BloodType B _) = True 
 donateTo _ _ = False -- Om de inte matchar är det kört 
 
+bmi :: Double -> Double -> (Double, String)
+bmi v l | bmiCalculator v l >= 30 = (bmiCalculator v l,"Svår övervikt")
+        | bmiCalculator v l < 30 && bmiCalculator v l > 25 = (bmiCalculator v l, "Övervikt")
+        | bmiCalculator v l < 25 && bmiCalculator v l > 18.5 = (bmiCalculator v l, "Normalvikt")
+        | otherwise = (bmiCalculator v l, "Undervikt")
+
+
+bmiCalculator :: Double -> Double -> Double
+bmiCalculator v l = v / (l * l)
 
 benjaminStrandberg = BloodType B Neg 
 
@@ -95,13 +113,15 @@ main = do
 
 invokeMenuScreen :: Gtk.Window  -> IO()
 invokeMenuScreen win = do 
-    Gtk.widgetDestroy win
+    #destroy win
+    
     
     
     menuState <- new Gtk.Window [#title := "Journal System"]
 
     #resize menuState 640 480
     #setPosition menuState Gtk.WindowPositionCenter 
+    on menuState #destroy main
 
     vertBox <- new Gtk.Box [#orientation := Gtk.OrientationVertical]
     #add menuState vertBox
@@ -109,39 +129,117 @@ invokeMenuScreen win = do
     addPatientbtn <- new Gtk.Button [#label := "Add patient"]
     #add vertBox addPatientbtn
 
+    on addPatientbtn #clicked $ do
+        invokeAddPatientWindow 
+
+    horBox1 <- new Gtk.Box [#orientation := Gtk.OrientationHorizontal]
+    #add vertBox horBox1
+    
+    #showAll menuState
+
+
+invokeAddPatientWindow :: IO()
+invokeAddPatientWindow = do
+    
+
+    addWindow <- new Gtk.Window [#title := "Journal System"]
+
+    #resize addWindow 320 240
+    #setPosition addWindow Gtk.WindowPositionCenter 
+    
+
+    vertBox <- new Gtk.Box [#orientation := Gtk.OrientationVertical]
+    #add addWindow vertBox
+
     horBox1 <- new Gtk.Box [#orientation := Gtk.OrientationHorizontal]
     #add vertBox horBox1
 
-    patient1Entry <- Gtk.entryNew
-    Gtk.containerAdd horBox1 patient1Entry
-    
+    horBox2 <- new Gtk.Box [#orientation := Gtk.OrientationHorizontal]
+    #add vertBox horBox2
 
-    patient2Entry <- Gtk.entryNew
-    Gtk.containerAdd horBox1 patient2Entry
-    
+    horBox3 <- new Gtk.Box [#orientation := Gtk.OrientationHorizontal]
+    #add vertBox horBox3
 
-    checkBtn <- new Gtk.Button [#label := "Check donation"]
-    #add vertBox checkBtn
+    horBox4 <- new Gtk.Box [#orientation := Gtk.OrientationHorizontal]
+    #add vertBox horBox4
 
-    label <- new Gtk.Label [#label := "asg"]
-    #add vertBox label
+    horBox5 <- new Gtk.Box [#orientation := Gtk.OrientationHorizontal]
+    #add vertBox horBox5
 
-    on checkBtn #clicked $ do
+    horBox6 <- new Gtk.Box [#orientation := Gtk.OrientationHorizontal]
+    #add vertBox horBox6
 
-        patient1 <- Gtk.entryGetText patient1Entry
-        patient2 <- Gtk.entryGetText patient2Entry
+    horBox7 <- new Gtk.Box [#orientation := Gtk.OrientationHorizontal]
+    #add vertBox horBox7
 
-        if patient1 == "Tommy Komo" && patient2 == "Benjamin Strandberg" then
-            if donateTo tommyKomo benjaminStrandberg then
-                set label [#label := "Tommy can donate to Benjamin"]
-            else set label [#label := "Tommy can't donate to Benjamin"]
-        else set label [#label := "The patients aren't in our database"]
+    fnameLabel <- new Gtk.Label [#label := "First Name: "]
+    #add horBox1 fnameLabel
+
+    lnameLabel <- new Gtk.Label [#label := "Last Name: "]
+    #add horBox2 lnameLabel
+
+    ageLabel <- new Gtk.Label   [#label := "Age:             "]
+    #add horBox3 ageLabel
+
+    gLabel <- new Gtk.Label     [#label := "Gender:      "]
+    #add horBox4 gLabel
+
+    hLabel <- new Gtk.Label     [#label := "Height:        "]
+    #add horBox5 hLabel
+
+    wLabel <- new Gtk.Label     [#label := "Weight:       "]
+    #add horBox6 wLabel
+
+    bTLabel <- new Gtk.Label    [#label := "Bloodtype:  "]
+    #add horBox7 bTLabel
+
+    fName <- Gtk.entryNew
+    Gtk.containerAdd horBox1 fName
+
+    lName <- Gtk.entryNew 
+    Gtk.containerAdd horBox2 lName
+
+    age <- Gtk.entryNew 
+    Gtk.containerAdd horBox3 age
+
+    gender <- Gtk.entryNew 
+    Gtk.containerAdd horBox4 gender
+
+    height <- Gtk.entryNew 
+    Gtk.containerAdd horBox5 height
+
+    weight <- Gtk.entryNew 
+    Gtk.containerAdd horBox6 weight
+
+    bloodType <- Gtk.entryNew 
+    Gtk.containerAdd horBox7 bloodType
+
+    addBtn <- new Gtk.Button [#label := "Add patient"]
+    #add vertBox addBtn
+
+
+    on addBtn #clicked $ do
         
-        
-        
+        f <- Gtk.entryGetText fName
+        l <- Gtk.entryGetText lName
+        a <- Gtk.entryGetText age
+        g <- Gtk.entryGetText gender
+        h <- Gtk.entryGetText height
+        w <- Gtk.entryGetText weight
+        b <- Gtk.entryGetText bloodType
+
+        let fileName = show f ++ show l ++ ".txt"
+        let text = show f ++ "\n" ++ show l ++ "\n" ++ show a ++ "\n" ++ show g ++ "\n" ++ show h ++ "\n" ++ show w ++ "\n" ++ show b
+
+        writeFile fileName text
+
+        #destroy addWindow
+    #showAll addWindow
+    
+    
+
+    
 
 
     
-    
-    #showAll menuState
 
